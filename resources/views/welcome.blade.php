f1352907ccd19e599899218d08cfe771a01982d8
siddharth018 <siddharthshukla089@gmail.com>
2021-03-30 18:05:34 +0530

    update code

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Botman Chatbot in Laravel - Real Programmer</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+    </body>
+   
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+    <script>
+        var botmanWidget = {
+            aboutText: 'Write Something',
+            introMessage: "✋ Hi! I'm form Real Programmer"
+        };
+    </script>
+   
+    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+       
+</html> 
